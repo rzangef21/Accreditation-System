d8d53c7a5f5bd5a784a1dad635ba5b60030e9030
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-05-29 22:44:39 +0700

    add display evaluasi for criteria 2

---
+++ resources/views/evaluasi/kriteria2.blade.php
@@ -0,0 +1,55 @@
+@extends('kriteria2.app')
+
+@section('title', 'Evaluasi')
+
+@section('content_header_title', 'Evaluasi')
+@section('content_header_subtitle', 'Formulir Pengisian')
+
+@section('content_body')
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('evaluasi2.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            {{-- Input Judul Penetapan --}}
+            <div class="form-group">
+                <label for="judul">Judul Evaluasi</label>
+                <input type="text" name="judul" id="judul" class="form-control">
+            </div>
+
+            {{-- Input teks panjang dengan Summernote --}}
+            <div class="form-group">
+                <label for="deskripsi">Deskripsi</label>
+                <textarea id="summernote" name="deskripsi" class="form-control" rows="6"></textarea>
+            </div>
+
+            {{-- Input link --}}
+            <div class="form-group">
+                <label for="link">Link</label>
+                <input type="url" name="link" class="form-control">
+            </div>
+
+            {{-- Upload file --}}
+            <div class="form-group">
+                <label for="dokumen">Unggah Dokumen</label>
+                <input type="file" name="dokumen" class="form-control-file">
+            </div>
+
+            <div class="mt-3">
+                <button type="submit" name="action" value="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="action" value="draft" class="btn btn-secondary">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            height: 200
+        });
+    });
+</script>
+@endpush
