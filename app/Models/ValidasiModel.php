d2593e35a0dd5aa136e6457e73e99c71c799fc8d
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-06-15 21:35:26 +0700

    update table validasi

--- app/Models/ValidasiModel.php
+++ app/Models/ValidasiModel.php
@@ -10,22 +10,48 @@ class ValidasiModel extends Model
     use HasFactory;
     protected $table = 'validasi';
     protected $primaryKey = 'id_validasi';
+    public $incrementing = true;
+    protected $keyType = 'int';
     public $timestamps = false;
-
     protected $fillable = [
+        'id_penetapan',
+        'id_pelaksanaan',
+        'id_evaluasi',
+        'id_pengendalian',
+        'id_peningkatan',
         'id_user',
-        'id_dokumen',
+        'level_validasi',
         'status',
         'komentar',
     ];
 
-    public function dokumen()
+    public function penetapan()
     {
-        return $this->belongsTo(DokumenModel::class, 'id_dokumen', 'id_dokumen');
+        return $this->belongsTo(PenetapanModel::class, 'id_penetapan', 'id_penetapan');
+    }
+
+    public function pelaksanaan()
+    {
+        return $this->belongsTo(PelaksanaanModel::class, 'id_pelaksanaan', 'id_pelaksanaan');
+    }
+
+    public function evaluasi()
+    {
+        return $this->belongsTo(EvaluasiModel::class, 'id_evaluasi', 'id_evaluasi');
+    }
+
+    public function pengendalian()
+    {
+        return $this->belongsTo(PengendalianModel::class, 'id_pengendalian', 'id_pengendalian');
+    }
+
+    public function peningkatan()
+    {
+        return $this->belongsTo(PeningkatanModel::class, 'id_peningkatan', 'id_peningkatan');
     }
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'id_user', 'id_user');
+        return $this->belongsTo(UserModel::class, 'id_user', 'id_user');
     }
 }
